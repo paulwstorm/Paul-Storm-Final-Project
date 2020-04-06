c65fac16201a52857e50f0c442e96cda91c47ed4
paulwstorm <anapaulorocket@gmail.com>
2020-04-06 19:51:25 -0400

    Added more images.

--- server/client/ps-final-project-app/src/components/intros/posts.js
+++ server/client/ps-final-project-app/src/components/intros/posts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react'
-import { Button, Card, Row, Col } from 'react-bootstrap'
+import { Button, Card, Row, Col, Image } from 'react-bootstrap'
 import './intro.css'
 
 
@@ -11,7 +11,12 @@ class IntroPosts extends Component {
             <div>
                 <h3 className="intro-header">Welcome to Posts!</h3>
                 <p className="intro-paragraph">On this page you can see read of the recent posts tailored to your reading level!</p>
-                <p className="intro-paragraph">These posts come from various Chinese social media and news outlets</p>
+                <p className="intro-paragraph">This is an example post:</p>
+                <Image className="sample-post" src="https://raw.githubusercontent.com/paulwstorm/Paul-Storm-Final-Project/master/server/images/Screen%20Shot%202020-04-06%20at%207.29.37%20PM.png"></Image>
+                <p className="intro-paragraph">
+                    For this post, the user is on the top left.
+                    Whatever the post, clicking on the top left hand corner with talk you the post source.
+                </p>
             </div>
         )
     }
