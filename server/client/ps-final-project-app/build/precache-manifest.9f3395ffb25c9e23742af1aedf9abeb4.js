613057fc4835be2754866c72d2d4bcde479d3b3b
paulwstorm <anapaulorocket@gmail.com>
2020-04-06 10:29:20 -0400

    Removed some more git ignore files.

---
+++ server/client/ps-final-project-app/build/precache-manifest.9f3395ffb25c9e23742af1aedf9abeb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee169267f6f8039d27555ad3517cf5ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe90648fe69c6bca3ec",
+    "url": "/static/css/main.5ce99ce1.chunk.css"
+  },
+  {
+    "revision": "59573587d0225d2c2476",
+    "url": "/static/js/2.5dc0de0f.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.5dc0de0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe90648fe69c6bca3ec",
+    "url": "/static/js/main.c1240953.chunk.js"
+  },
+  {
+    "revision": "e93dcef64e9ee009dbe2",
+    "url": "/static/js/runtime-main.47902c7a.js"
+  }
+]);
