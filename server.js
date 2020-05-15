510df1b1d172709c31514e93e86ca203e549e61d
paulwstorm <anapaulorocket@gmail.com>
2020-05-15 14:17:43 -0400

    Checking server routes.

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.post("/backend/userinfo", (req, res) => {
 })
 
 app.post('/login', passport.authenticate('login', {
-  successRedirect: '/POSTS',
+  successRedirect: '/posts',
   failureRedirect: '/'
 }));
 
