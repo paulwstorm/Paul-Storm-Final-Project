6fe5a2d27fc9d622ff6e27200dfb8eb67f36aeec
paulwstorm <anapaulorocket@gmail.com>
2020-05-15 14:34:21 -0400

    Fixed res

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ if (process.env.NODE_ENV === 'production') {
   const path = require('path');
 
   app.get('*', (req, res) => {
-    res.json(req.url)
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
