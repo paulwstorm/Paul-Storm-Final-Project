27028baa6d0898bbbc9bef5d5f86a8f7f1ae0841
paulwstorm <anapaulorocket@gmail.com>
2020-05-16 18:43:57 -0400

    Added sorting to dictionary entries.

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get("/backend/wordsearch", (req,res) => {
     const regExQuery = new RegExp(`\b(\w*${query}\w*)\b`, "i")
   
     Dictionary
-      .find({english: { $regex: regExQuery}})
+      .find({english: { $regex: `\b(\w*${query}\w*)\b`}})
       // .limit(10)
       .exec((err, result) => {
         if (err) {
